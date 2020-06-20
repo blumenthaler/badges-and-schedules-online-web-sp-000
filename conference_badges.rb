18993130f79d534ff6a5753ebe3c0f9f5b3e6dd7
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-20 17:50:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-conf_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+# speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator
+def batch_badge_creator(speakers)
