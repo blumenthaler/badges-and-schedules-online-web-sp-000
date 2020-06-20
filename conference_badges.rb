85da68a9e3d83578855c14bdb905050d03346dcc
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-20 17:58:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,8 +16,8 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(speakers, rooms)
-  room_assignments = []
-  speakers.each do |name|
+  #room_assignments = []
+ # speakers.each do |name|
   
 end
 
