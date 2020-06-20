b8b7569434bffb765245659c92de88cc3547f92e
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-20 17:55:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,8 @@ end
 
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
+rooms = [1, 2, 3, 4, 5, 6, 7]
+
 def batch_badge_creator(speakers)
   new_badges = []
   speakers.each do |name|
